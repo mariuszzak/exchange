be6979089894fa5ec7170788a4743012a2cffc24
Mariusz <mariuszzak21@gmail.com>
2020-05-20 14:11:54 +0200

    Extract EventInputValidator

--- lib/exchange.ex
+++ lib/exchange.ex
@@ -6,89 +6,34 @@ defmodule Exchange do
 
   use Agent
 
+  alias Exchange.EventInputValidator
+
   @spec start_link :: Agent.on_start()
   def start_link do
     initial_value = []
     Agent.start_link(fn -> initial_value end)
   end
 
-  @typep validation_error ::
-           :invalid_instruction_type
-           | :invalid_side_type
-           | :invalid_quantity_value
-           | :invalid_quantity_type
-           | :invalid_price_level_index_value
-           | :invalid_price_level_index_type
-           | :invalid_price_value
-           | :invalid_price_type
+  @type send_instruction_result ::
+          :ok | {:error, EventInputValidator.validation_error() | :exchange_is_not_running}
 
-  @spec send_instruction(exchange_pid :: pid(), event :: map()) ::
-          :ok | {:error, validation_error() | :exchange_is_not_running}
+  @spec send_instruction(exchange_pid :: pid(), event :: map()) :: send_instruction_result()
   def send_instruction(exchange_pid, event) do
-    with :ok <- validate_event(event),
+    with :ok <- EventInputValidator.call(event),
          :ok <- validate_exchange_state(exchange_pid),
          :ok <- Agent.update(exchange_pid, &apply_event/1) do
       :ok
     end
   end
 
-  defp validate_event(event) do
-    with :ok <- validate_instruction(event),
-         :ok <- validate_side(event),
-         :ok <- validate_quantity(event),
-         :ok <- validate_price_level_index(event),
-         :ok <- validate_price(event) do
-      :ok
-    end
-  end
-
-  defp apply_event(state) do
-    state
-  end
-
-  defp validate_instruction(%{instruction: instruction})
-       when instruction in [:new, :update, :delete],
-       do: :ok
-
-  defp validate_instruction(_), do: {:error, :invalid_instruction_type}
-
-  defp validate_side(%{side: side})
-       when side in [:bid, :ask],
-       do: :ok
-
-  defp validate_side(_), do: {:error, :invalid_side_type}
-
-  defp validate_quantity(%{quantity: quantity}) when is_integer(quantity) and quantity < 1,
-    do: {:error, :invalid_quantity_value}
-
-  defp validate_quantity(%{quantity: quantity}) when is_integer(quantity), do: :ok
-
-  defp validate_quantity(_), do: {:error, :invalid_quantity_type}
-
-  defp validate_price_level_index(%{price_level_index: price_level_index})
-       when is_integer(price_level_index) and price_level_index < 1,
-       do: {:error, :invalid_price_level_index_value}
-
-  defp validate_price_level_index(%{price_level_index: price_level_index})
-       when is_integer(price_level_index),
-       do: :ok
-
-  defp validate_price_level_index(_), do: {:error, :invalid_price_level_index_type}
-
-  defp validate_price(%{price: price})
-       when is_number(price) and price < 1,
-       do: {:error, :invalid_price_value}
-
-  defp validate_price(%{price: price})
-       when is_number(price),
-       do: :ok
-
-  defp validate_price(_), do: {:error, :invalid_price_type}
-
   defp validate_exchange_state(exchange_pid) do
     case Process.alive?(exchange_pid) do
       true -> :ok
       false -> {:error, :exchange_is_not_running}
     end
+  end
+
+  defp apply_event(state) do
+    state
   end
 end
---
+++ lib/exchange/event_input_validator.ex
@@ -0,0 +1,63 @@
+defmodule Exchange.EventInputValidator do
+  @moduledoc false
+
+  @type validation_error ::
+          :invalid_instruction_type
+          | :invalid_side_type
+          | :invalid_quantity_value
+          | :invalid_quantity_type
+          | :invalid_price_level_index_value
+          | :invalid_price_level_index_type
+          | :invalid_price_value
+          | :invalid_price_type
+
+  @spec call(map()) :: :ok | validation_error()
+  def call(event) do
+    with :ok <- validate_instruction(event),
+         :ok <- validate_side(event),
+         :ok <- validate_quantity(event),
+         :ok <- validate_price_level_index(event),
+         :ok <- validate_price(event) do
+      :ok
+    end
+  end
+
+  defp validate_instruction(%{instruction: instruction})
+       when instruction in [:new, :update, :delete],
+       do: :ok
+
+  defp validate_instruction(_), do: {:error, :invalid_instruction_type}
+
+  defp validate_side(%{side: side})
+       when side in [:bid, :ask],
+       do: :ok
+
+  defp validate_side(_), do: {:error, :invalid_side_type}
+
+  defp validate_quantity(%{quantity: quantity}) when is_integer(quantity) and quantity < 1,
+    do: {:error, :invalid_quantity_value}
+
+  defp validate_quantity(%{quantity: quantity}) when is_integer(quantity), do: :ok
+
+  defp validate_quantity(_), do: {:error, :invalid_quantity_type}
+
+  defp validate_price_level_index(%{price_level_index: price_level_index})
+       when is_integer(price_level_index) and price_level_index < 1,
+       do: {:error, :invalid_price_level_index_value}
+
+  defp validate_price_level_index(%{price_level_index: price_level_index})
+       when is_integer(price_level_index),
+       do: :ok
+
+  defp validate_price_level_index(_), do: {:error, :invalid_price_level_index_type}
+
+  defp validate_price(%{price: price})
+       when is_number(price) and price < 1,
+       do: {:error, :invalid_price_value}
+
+  defp validate_price(%{price: price})
+       when is_number(price),
+       do: :ok
+
+  defp validate_price(_), do: {:error, :invalid_price_type}
+end
